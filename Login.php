21f61eba72feb698b4d917b1be39330db8d76c07
Whitnu12 <whitnu@gmail.com>
2021-12-02 22:20:53 +0800

    contact update

--- Login.php
+++ Login.php
@@ -60,14 +60,14 @@
         <!-- Awal Navbar -->
         <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #396eb0">
           <div class="container">
-            <a class="navbar-brand" href="#"> <img src="src/photo/RecarIcon22.png" alt="wN" width="60" height="50" style="margin-right: 20px;;"/>Re<b>Car</b></a>
+            <a class="navbar-brand" href="index.php"> <img src="src/photo/RecarIcon22.png" alt="wN" width="60" height="50" style="margin-right: 20px;;"/>Re<b>Car</b></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
               <div class="navbar-nav ms-auto">
                 <a class="nav-link" href="#about">Sewa Mobil</a>
-                <a class="nav-link" href="#gallery">Hubungi Kami</a>
+                <a class="nav-link" href="contactus.html">Hubungi Kami</a>
                 <a class="nav-link" href="Login.html" style="color: white"><b>Masuk</b></a>
               </div>
             </div>
